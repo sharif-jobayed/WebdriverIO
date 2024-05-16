520d4f1ec638861f157021a4c64ae98549e773c5
sharif-jobayed <sharif.jobayed@gmail.com>
2024-05-16 22:38:59 +0600

    Element & Page objects formatted

--- tests/pages/homePage.js
+++ tests/pages/homePage.js
@@ -7,8 +7,18 @@ class HomePage extends BasePage {
 	constructor(pageURL, pageName) {
 		super(pageURL, pageName);
 
-		this.ABTestingLink = new BaseElement(`//a[@href="/abtest"]`, `ABTestingLink`);
-		this.AddRemoveElementsLink = new BaseElement(`//a[@href="/add_remove_elements/"]`, `AddRemoveElementsLink`);
+		this.ABTestingLink = new BaseElement(
+			`//a[@href="/abtest"]`,
+			`ABTestingLink`
+		);
+		this.AddRemoveElementsLink = new BaseElement(
+			`//a[@href="/add_remove_elements/"]`,
+			`AddRemoveElementsLink`
+		);
+		this.BasicAuthLink = new BaseElement(
+			`//a[@href="/basic_auth"]`,
+			`BasicAuthLink`
+		);
 	}
 
 }
--- tests/utils/pages.js
+++ tests/utils/pages.js
@@ -1,15 +1,24 @@
 const HomePage = require('../pages/homePage.js');
-const ABTestingPage = require('../pages/ABTestingPage.js');
-const AddRemoveElementsPage = require('../pages/AddRemoveElementsPage.js');
+const ABTestingPage = require('../pages/abTestingPage.js');
+const AddRemoveElementsPage = require('../pages/addRemoveElementsPage.js');
 
 
 class Pages {
 
 	constructor() {
 
-		this.HomePage = new HomePage(`https://the-internet.herokuapp.com`, `HomePage`);
-		this.ABTestingPage = new ABTestingPage(`https://the-internet.herokuapp.com/abtest` , `ABTestingPage`);
-		this.AddRemoveElementsPage = new AddRemoveElementsPage(`https://the-internet.herokuapp.com/add_remove_elements`, `AddRemoveElementsPage`);
+		this.HomePage = new HomePage(
+			`https://the-internet.herokuapp.com`,
+			`HomePage`
+		);
+		this.ABTestingPage = new ABTestingPage(
+			`https://the-internet.herokuapp.com/abtest` ,
+			`ABTestingPage`
+		);
+		this.AddRemoveElementsPage = new AddRemoveElementsPage(
+			`https://the-internet.herokuapp.com/add_remove_elements`,
+			`AddRemoveElementsPage`
+		);
 
 	}
 
