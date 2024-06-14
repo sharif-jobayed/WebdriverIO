ef062b18e201d265e5a1faf4d6ac82bfb1bad9d9
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-15 01:18:45 +0600

    Project cleaned

--- wdio.conf.js
+++ wdio.conf.js
@@ -146,7 +146,7 @@ exports.config = {
         // <boolean> fail if there are any undefined or pending steps
         strict: false,
         // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: '',
+        tagExpression: 'Test-0001',
         // <number> timeout for step definitions
         timeout: 60000,
         // <boolean> Enable this config to treat undefined definitions as warnings.
