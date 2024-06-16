625c6cef2ab7734ef7d0d53c4ea91cf1fd22f0d0
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-16 18:28:13 +0600

    Update BasePage

--- src/pages/basePage.js
+++ src/pages/basePage.js
@@ -1,7 +1,24 @@
+const { browser } = require('webdriverio');
+
 class BasePage {
   constructor(pageURL, pageName) {
     this.url = pageURL;
     this.name = pageName;
+  }
+
+  // Navigate to the page URL
+  async navigate() {
+    await browser.url(this.url);
+  }
+
+  // Get the current URL of the browser
+  async currentURL() {
+    return await browser.getUrl();
+  }
+
+  // Alias for navigate
+  async open() {
+    await this.navigate();
   }
 }
 
