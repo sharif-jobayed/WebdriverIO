0d6be002f458943c6930f451f78a0fdccffdf0e1
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-16 17:45:12 +0600

    Add $,$$ to the BaseElement

--- src/elements/baseElement.js
+++ src/elements/baseElement.js
@@ -1,3 +1,5 @@
+const {$, $$} = require('@wdio/globals');
+
 class BaseElement {
 	constructor(elementLocator, elementName) {
 		this.locator = $(elementLocator);
@@ -5,6 +7,19 @@ class BaseElement {
 		this.name = elementName;
 	}
 
+	async doClick() {
+		console.log(`Operating on ${this.name}`);
+		return (await this.locator).click();
+	}
+	async elementText() {
+		console.log(`Operating on ${this.name}`);
+		return this.locator.getText();
+	}
+	async clearAndType(value) {
+		console.log(`Operating on ${this.name}`);
+		(await this.locator).clearValue();
+		return (await this.locator).addValue(value);
+	}
 }
 
-module.exports = new BaseElement();
+module.exports = BaseElement;
