e7a1ad57afd3ff3118ffb95c6a7dc46bbac5d439
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-16 18:03:09 +0600

    Add HomePage

---
+++ src/pages/homePage.js
@@ -0,0 +1,9 @@
+const BasePage = require('../pages/basePage.js');
+
+class HomePage extends BasePage {
+  constructor(pageURL, pageName) {
+    super(pageURL, pageName);
+  }
+}
+
+module.exports = HomePage;
