ccde7065f507706e64bb0aa5f976a47a7112519c
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-16 18:26:16 +0600

    Update BaseElement

--- src/elements/baseElement.js
+++ src/elements/baseElement.js
@@ -1,25 +1,72 @@
-const {$, $$} = require('@wdio/globals');
+const { $, $$ } = require('@wdio/globals');
 
 class BaseElement {
-	constructor(elementLocator, elementName) {
-		this.locator = $(elementLocator);
-		this.locators = $$(elementLocator);
-		this.name = elementName;
-	}
+  constructor(elementLocator, elementName) {
+    this.locator = $(elementLocator);
+    this.locators = $$(elementLocator);
+    this.name = elementName;
+  }
 
-	async doClick() {
-		console.log(`Operating on ${this.name}`);
-		return (await this.locator).click();
-	}
-	async elementText() {
-		console.log(`Operating on ${this.name}`);
-		return this.locator.getText();
-	}
-	async clearAndType(value) {
-		console.log(`Operating on ${this.name}`);
-		(await this.locator).clearValue();
-		return (await this.locator).addValue(value);
-	}
+  async doClick() {
+    try {
+      console.log(`Clicking on ${this.name}`);
+      await (await this.locator).click();
+    } catch (error) {
+      console.error(`Error clicking on ${this.name}: ${error}`);
+      throw error;
+    }
+  }
+
+  async elementText() {
+    try {
+      console.log(`Getting text from ${this.name}`);
+      return await (await this.locator).getText();
+    } catch (error) {
+      console.error(`Error getting text from ${this.name}: ${error}`);
+      throw error;
+    }
+  }
+
+  async clearAndType(value) {
+    try {
+      console.log(`Clearing and typing into ${this.name}`);
+      await (await this.locator).clearValue();
+      await (await this.locator).addValue(value);
+    } catch (error) {
+      console.error(`Error clearing and typing into ${this.name}: ${error}`);
+      throw error;
+    }
+  }
+
+  async isDisplayed() {
+    try {
+      console.log(`Checking if ${this.name} is displayed`);
+      return await (await this.locator).isDisplayed();
+    } catch (error) {
+      console.error(`Error checking if ${this.name} is displayed: ${error}`);
+      throw error;
+    }
+  }
+
+  async isEnabled() {
+    try {
+      console.log(`Checking if ${this.name} is enabled`);
+      return await (await this.locator).isEnabled();
+    } catch (error) {
+      console.error(`Error checking if ${this.name} is enabled: ${error}`);
+      throw error;
+    }
+  }
+
+  async getAttribute(attributeName) {
+    try {
+      console.log(`Getting attribute '${attributeName}' from ${this.name}`);
+      return await (await this.locator).getAttribute(attributeName);
+    } catch (error) {
+      console.error(`Error getting attribute '${attributeName}' from ${this.name}: ${error}`);
+      throw error;
+    }
+  }
 }
 
 module.exports = BaseElement;
