7230bdee35b675001b3c145e11446e239ed1f1bd
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-22 19:53:23 +0600

    Add addRemoveElementsPage Remove getElements()

--- src/utils/converters.js
+++ src/utils/converters.js
@@ -1,10 +1,12 @@
 import { MainPage } from "../pages/mainPage.js";
 import { ABTestingPage } from "../pages/abTestingPage.js";
+import { AddRemoveElementsPage } from "../pages/addRemoveElementsPage.js";
 
 class Converters {
 	constructor() {
 		this.mainPage = new MainPage();
 		this.abTestingPage = new ABTestingPage();
+		this.addRemoveElementsPage = new AddRemoveElementsPage();
 	}
 
 	async getPage(value) {
@@ -13,14 +15,11 @@ class Converters {
 				return this.mainPage;
 			case this.abTestingPage.pageName.includes(value):
 				return this.abTestingPage;
+			case this.addRemoveElementsPage.pageName.includes(value):
+				return this.addRemoveElementsPage;
 			default:
 				throw new Error(`Invalid page request: ${value}`);
 		}
-	}
-
-	async getElement(value) {
-		// To set yet...
-		throw new Error(`Invalid element request: ${value}`);
 	}
 }
 
