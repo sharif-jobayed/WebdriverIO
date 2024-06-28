2359d7dec54c5b5a4b87a02f5a96876a1ae4a791
sharif-jobayed <sharif.jobayed@gmail.com>
2024-06-29 03:37:39 +0600

    Remove object

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,14 +5,8 @@ const timeoutInMS = {
 	maximum: 70000,
 }
 
-const pageNames = {
-	mainPage: `Main`,
-	abTestingPage: `A/B Testing`,
-	addRemoveElementsPage: `Add/Remove Elements`,
-}
-
 const shortText = {
 	abTestHeading: `A/B Test Control`,
 }
 
-export { timeoutInMS, pageNames, shortText }
+export { timeoutInMS, shortText }
