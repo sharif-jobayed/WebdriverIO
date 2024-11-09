43aa4e2079b115bc9b3a37298c7ba14ea3f18c5a
sharif-jobayed <sharif.jobayed@gmail.com>
2024-11-10 02:25:58 +0600

    Refactor code

---
+++ src/pages/CustomerLoginPage.js
@@ -0,0 +1,9 @@
+import {BasePage} from "../framework/BasePage.js";
+
+class CustomerLoginPage extends BasePage {
+    constructor(pageURL, pageName) {
+        super(pageURL, pageName);
+    }
+}
+
+export {CustomerLoginPage}
