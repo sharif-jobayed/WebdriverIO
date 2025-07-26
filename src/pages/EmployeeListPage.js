02d684275a31bc841aff88fc664d654051fba0ec
sharif-jobayed <sharif.jobayed@gmail.com>
2025-07-27 00:33:42 +0600

    Simplified isEmployeeInList()

--- src/pages/EmployeeListPage.js
+++ src/pages/EmployeeListPage.js
@@ -11,7 +11,7 @@ class EmployeeList extends BasePage {
 		this.searchBtn = new BaseElement(`//button[@type='submit']`);
 		this.resultCount = new BaseElement(`//div[contains(@class,'orangehrm-horizontal-padding')]/span[contains(@class,'oxd-text')]`);
 		this.empTableHeader = new BaseElement(`//div[@role='table']/div[1]`);
-		this.empIdsColumn = new BaseElement(`//div[contains(@class, 'oxd-table-row')]/div[contains(@class, 'oxd-table-cell')][2]`);
+		this.empIdsColumn = new BaseElement(`//div[contains(@class, 'oxd-table-row')]/div[contains(@class, 'oxd-table-cell')][2]`).elLocators;
 		this.profileEditBtn = new BaseElement(`//div[@class='oxd-table-cell-actions']//i[contains(@class,'bi-pencil-fill')]`);
 	}
 
@@ -24,39 +24,23 @@ class EmployeeList extends BasePage {
 		await this.addBtn.doClick();
 	}
 
-	async enterEmployeeIdAndSearch(empId) {
-		if (empId) {
-			await this.empIdSearchField.clearAndType(empId);
-			await this.searchBtn.doClick();
-			await this.resultCount.isVisible();
-		} else {
-			const employeeId = await this.getEmployeeCreds().then(data => data.employeeId);
-			await this.empIdSearchField.clearAndType(employeeId);
-			await this.searchBtn.doClick();
-			await this.resultCount.isVisible();
-		}
+	async enterEmployeeIdAndSearch() {
+		const employeeId = await this.getEmployeeCreds().then(data => data.employeeId);
+		await this.empIdSearchField.clearAndType(employeeId);
+		await this.searchBtn.doClick();
+		await this.resultCount.isVisible();
 	}
 
-	async isEmployeeInList(empId) {
-		if (empId) {
-			const empIds = this.empIdsColumn.elLocators;
-			for (let i = 0; i < await empIds.length; i++) {
-				const empIdText = await empIds[i].getText();
-				if (empIdText === empId.toString()) {
-					return true;
-				}
+	async isEmployeeInList() {
+		const employeeId = await this.getEmployeeCreds().then(data => data.employeeId);
+
+		for (let i = 0; i < await this.empIdsColumn.length; i++) {
+			const empIdText = await this.empIdsColumn[i].getText();
+			if (empIdText == employeeId) {
+				return true;
+			} else {
+				return false;
 			}
-			return false;
-		} else {
-			const employeeId = await this.getEmployeeCreds().then(data => data.employeeId);
-			const empIds = this.empIdsColumn.elLocators;
-			for (let i = 0; i < await empIds.length; i++) {
-				const empIdText = await empIds[i].getText();
-				if (empIdText === employeeId) {
-					return true;
-				}
-			}
-			return false;
 		}
 	}
 
